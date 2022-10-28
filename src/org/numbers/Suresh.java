c20de4001e8f51434b31e3faaef52e1782d9254b
suresht123 <janeshsuresh123@gmail.com>
2022-10-28 12:32:15 +0530

    task completed

---
+++ src/org/numbers/Suresh.java
@@ -0,0 +1,21 @@
+package org.numbers;
+
+import java.nio.charset.MalformedInputException;
+
+public class Suresh {
+		
+		public  void gitcreation (){
+		
+		System.out.println("git creation done");
+		}
+		public static void main(String[] args) {
+		
+		
+		Suresh s = new Suresh();
+		
+		s.gitcreation();
+		
+	
+	}
+
+}
