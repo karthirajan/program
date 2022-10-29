7b1f874fb88855726e58e4d8da0aba216d5b15d3
thikash <aathipirakash0244@gmail.com>
2022-10-29 15:57:17 +0530

    I've completed task

--- src/org/numbers/Abishek.java
+++ src/org/numbers/Abishek.java
@@ -2,6 +2,5 @@ package org.numbers;
 
 public class Abishek {
      public static void main(String[] args) {
-	  System.out.println("task completed");
-}
+	  System.out.println("task completed");}
 }
