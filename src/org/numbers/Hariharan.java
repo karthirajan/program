83aecd61a6c0c9ccc5ccb57c3acb70646a16f4fa
hariharan.k <hariharanscorpian@gmail.com>
2022-10-29 17:32:27 +0530

    task completed

---
+++ src/org/numbers/Hariharan.java
@@ -0,0 +1,27 @@
+package org.numbers;
+
+public class Hariharan {
+
+	public void saving() {
+		System.out.println("saving regular");
+			}
+			public void current() {
+				System.out.println("current");
+			}
+				public void salary() {
+					System.out.println("salary");
+				}
+				public void joint() {
+					System.out.println("joint");
+					
+				}
+				public static void main(String[] args) {
+					Hariharan h= new Hariharan();
+					h.current();
+					h.salary();
+					h.saving();
+				h.joint();
+
+	}
+
+}
